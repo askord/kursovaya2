25b8fa6fb3a5b4dd3fc5171be301395f40e5e3e3
askord <kumiteya173@yandex.ru>
2018-10-01 21:29:34 +0400

    #1 сделал регистрацию с админкой

---
+++ resources/views/form.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<hmtl lang="ru">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport"
+content=width=device-width, user-scalable-no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0>
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>document</title>
+<body>
+<h1>Send mail<h1>
+{!! Form::open(['url' => 'foo/bar']) !!}
+    {{Form::text('name')}}
+	{{Form::text('email')}}
+	{{Form::textarea('msg')}}
+	{{Form::submit('send')}}
+{!! Form::close() !!}
+</body>
+</html>
